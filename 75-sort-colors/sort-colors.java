85347b00e47399b8f5325589f68a7a0b6e77263b
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2024-11-25 15:20:23 -0800

    Time: 0 ms (100.00%) | Memory: 41.7 MB (67.37%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,35 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        quicksort(nums,0,n-1);
-    }
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
 
-    private void quicksort(int[] nums, int low , int high){
-        if(low<high){
-            int pivot=partition(nums, low, high);
-            quicksort(nums,low, pivot-1);
-            quicksort(nums, pivot+1,high);
-        }
-    }
-
-    private int partition(int[] nums, int low, int high){
-        int pivot=nums[high];
-        int i=low-1;
-
-        for(int j=low;j<high;j++){
-            if(nums[j]<pivot){
-                i++;
-                swap(nums,i,j);
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums,mid,high);
+                high--;
             }
         }
-
-        swap(nums,i+1,high);
-        return (i+1);
     }
 
-    private void swap(int[] nums, int i,int j){
-        int temp=nums[j];
-        nums[j]=nums[i];
-        nums[i]=temp;
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
