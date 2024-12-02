2a0814b907d3f15ef31cb54a8c8f2dd6f0280e6b
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2024-12-01 16:34:26 -0800

    Time: 13 ms (75.93%) | Memory: 48.7 MB (43.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] nums, int k) { 
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums)
-            map.put(num,map.getOrDefault(num,0)+1);
         
-    PriorityQueue<Map.Entry<Integer,Integer>> topKElements =new PriorityQueue<>(
-        (a,b)->a.getValue()-b.getValue()
-    );
+        for(int num:nums)
+            map.put(num, map.getOrDefault(num,0)+1);
 
-    for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-        topKElements.add(entry);
-        if(topKElements.size()>k)
-            topKElements.poll();
-    }
+        PriorityQueue<Map.Entry<Integer,Integer>> topKElements=new PriorityQueue<>(
+            (a,b)->a.getValue()-b.getValue()
+        );
 
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            topKElements.add(entry);
+            if(topKElements.size()>k)
+                topKElements.poll();
+        }
 
+        int[] topNumbers=new int[k];
+        for(int i=0;i<k;i++)
+            topNumbers[i]=topKElements.poll().getKey();
 
-    int[] topNumbers=new int[k];
-    for(int i=0;i<k;i++){
-        topNumbers[i]=topKElements.poll().getKey();
-    }
-
-    return topNumbers;
+        return topNumbers;
     }
 }
