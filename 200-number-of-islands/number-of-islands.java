c873f0fa9f505d768c340483c09dd8b4b3960262
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2025-02-15 13:04:13 -0800

    Time: 3 ms (86.72%) | Memory: 49.5 MB (70.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,35 @@
 class Solution {
+    public void dfs(char[][] grid, int i, int j) {
+        // Boundary check & water check
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        
+        // Mark this cell as visited by changing '1' to '0'
+        grid[i][j] = '0';
+
+        // Explore all four directions
+        dfs(grid, i + 1, j); // Down
+        dfs(grid, i - 1, j); // Up
+        dfs(grid, i, j + 1); // Right
+        dfs(grid, i, j - 1); // Left
+    }
+
     public int numIslands(char[][] grid) {
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(grid,i,j);
+        if (grid == null || grid.length == 0) return 0;
+
+        int numIslands = 0;
+        
+        // Traverse the grid
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                // If a land cell is found, start DFS and count an island
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return count;
-    }
-
-    private void dfs(char[][] grid,int i,int j){
-        if(i<0 || j<0 || i==grid.length || j==grid[0].length)
-            return;
-
-        if(grid[i][j]!='1')
-            return;
-        else
-            grid[i][j]='2';
-
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
+        return numIslands;
     }
 }
