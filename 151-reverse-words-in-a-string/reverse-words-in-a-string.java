6a2524336c366a7967b12f27fe9e945429d4a956
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2025-04-16 22:38:59 -0700

    Time: 8 ms (39.82%) | Memory: 42.6 MB (94.71%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,59 +1,28 @@
 class Solution {
-    public StringBuilder trimSpaces(String s) {
-        int left = 0, right = s.length() - 1;
-        // remove leading spaces
-        while (left <= right && s.charAt(left) == ' ') ++left;
-
-        // remove trailing spaces
-        while (left <= right && s.charAt(right) == ' ') --right;
-
-        // reduce multiple spaces to single one
-        StringBuilder sb = new StringBuilder();
-        while (left <= right) {
-            char c = s.charAt(left);
-
-            if (c != ' ') sb.append(c);
-            else if (sb.charAt(sb.length() - 1) != ' ') sb.append(c);
-
-            ++left;
-        }
-        return sb;
-    }
-
-    public void reverse(StringBuilder sb, int left, int right) {
-        while (left < right) {
-            char tmp = sb.charAt(left);
-            sb.setCharAt(left++, sb.charAt(right));
-            sb.setCharAt(right--, tmp);
-        }
-    }
-
-    public void reverseEachWord(StringBuilder sb) {
-        int n = sb.length();
-        int start = 0, end = 0;
-
-        while (start < n) {
-            // go to the end of the word
-            while (end < n && sb.charAt(end) != ' ') ++end;
-            // reverse the word
-            reverse(sb, start, end - 1);
-            // move to the next word
-            start = end + 1;
-            ++end;
-        }
-    }
-
     public String reverseWords(String s) {
-        // converst string to string builder
-        // and trim spaces at the same time
-        StringBuilder sb = trimSpaces(s);
+        int left=0;
+        int right=s.length()-1;
+        while(left<=right && s.charAt(left)==' ')   left++;
 
-        // reverse the whole string
-        reverse(sb, 0, sb.length() - 1);
+        while(left<=right && s.charAt(right)==' ')   right--;
 
-        // reverse each word
-        reverseEachWord(sb);
-
-        return sb.toString();
+        Deque<String> d=new ArrayDeque();
+        StringBuilder word=new StringBuilder();
+        while(left<=right)
+        {
+            char c=s.charAt(left);
+            if(word.length()!=0 && c==' ')
+            {
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            }
+            else if(c!=' ')
+            {
+                word.append(c);
+            }
+            left++;
+        }
+        d.offerFirst(word.toString());
+        return String.join(" ",d);
     }
 }
