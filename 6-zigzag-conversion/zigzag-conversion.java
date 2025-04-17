e487ad721d18dbbdacfc0f9b74f066e1c74f74ed
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2025-04-16 21:14:38 -0700

    Time: 29 ms (10.41%) | Memory: 46.8 MB (5.11%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1)  return s;
+
+        int n=s.length();
+        int sections=(int) Math.ceil(n/(2*numRows-2.0));
+
+        int numCols=sections*(numRows-1);
+
+        char[][] matrix=new char[numRows][numCols];
+        for(char[] row: matrix)
+        {
+            Arrays.fill(row,' ');
+        }
+
+        int currRow=0; int currCol=0;
+        int currStringIndex=0;
+        while(currStringIndex<n)
+        {
+            while(currRow<numRows && currStringIndex<n)
+            {
+                matrix[currRow][currCol]=s.charAt(currStringIndex);
+                currRow++;
+                currStringIndex++;
+            }
+            currCol++;
+            currRow-=2;
+            while(currRow> 0 && currCol < numCols && currStringIndex<n)
+            {
+                matrix[currRow][currCol]=s.charAt(currStringIndex);
+                currRow--;
+                currCol++;
+                currStringIndex++;
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char[] row:matrix)
+        {
+            for(char character:row)
+            {
+                if(character!=' ')
+                    sb.append(character);
+            }
+        }
+
+        return sb.toString();
+    }
+}
