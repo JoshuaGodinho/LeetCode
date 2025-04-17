5ab49f8b01d242557291f36f1ba3669b1f058be8
Joshua Godinho <69786791+JoshuaGodinho@users.noreply.github.com>
2025-04-17 16:08:42 -0700

    Time: 1 ms (93.61%) | Memory: 41.7 MB (24.36%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int leftBound=s.length(); 
+        int rightBound=t.length();
+        int pLeft=0;
+        int pRight=0;
+
+        while(pLeft< leftBound && pRight<rightBound)
+        {
+            if(s.charAt(pLeft)==t.charAt(pRight))
+            {
+                pLeft++;
+            }
+            pRight++;
+        }
+
+        return pLeft==leftBound;
+    }
+}
